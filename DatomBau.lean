3cb09405859a135c1596ff9c808b401445499abe
tobiasosborne <tobias.osborne@itp.uni-hannover.de>
2025-11-24 13:30:36 +0100

    fixed some things

--- DatomBau.lean
+++ DatomBau.lean
@@ -1,3 +1,15 @@
 -- This module serves as the root of the `DatomBau` library.
 -- Import modules here that should be built as part of the library.
+
 import DatomBau.Basic
+import DatomBau.Core
+import DatomBau.Index
+import DatomBau.Schema
+import DatomBau.Store
+import DatomBau.Transactor
+import DatomBau.Query
+import DatomBau.Util
+
+namespace DatomBau
+
+end DatomBau
--- Main.lean
+++ Main.lean
@@ -1,4 +1,4 @@
 import DatomBau
 
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  IO.println "DatomBau CLI starting..."
